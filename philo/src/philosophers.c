ddbd728580adba339b026cc31ec6baba97c19e6e
Vitor Sergio <vitorsergio111@gmail.com>
2022-10-11 00:14:35 -0300

    testing mem thread

--- philo/src/philosophers.c
+++ philo/src/philosophers.c
@@ -6,15 +6,16 @@
 /*   By: vsergio <vsergio@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 14:17:36 by vsergio           #+#    #+#             */
-/*   Updated: 2022/10/10 23:43:06 by Vitor            ###   ########.fr       */
+/*   Updated: 2022/10/11 00:14:25 by Vitor            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+int kikos = 5;
 
 #include "../include/philosophers.h"
-void	*test()
+void	*test(void *value)
 {
-	int value = 3;
-	printf("Valor: %i\n", ++value);
+	int *rato = value; //vai castar pra int * e apontar pro msm valor na memoria dos threads
+	printf("Valor: %i\n", ++*rato);
 	printf("Running\n");
 	sleep(5);
 	printf("Ending\n");
@@ -25,10 +26,11 @@ int main(void)
 {
 	pthread_t t1;
 	pthread_t t2;
+	int value = 3;
 
-	if (pthread_create(&t1, NULL, &test, NULL) != 0) //criamos a thread, passamos ponteiro da variavel e a funcao p/ ser chamada
+	if (pthread_create(&t1, NULL, &test, &value) != 0) //criamos a thread, passamos ponteiro da variavel e a funcao p/ ser chamada
 		write(2, "Error\n", 6);
-	if (pthread_create(&t2, NULL, &test, NULL) != 0) //criamos a thread, passamos ponteiro da variavel e a funcao p/ ser chamada
+	if (pthread_create(&t2, NULL, &test, &value) != 0) //criamos a thread, passamos ponteiro da variavel e a funcao p/ ser chamada
 		write(2, "Error\n", 6);
 	if (pthread_join(t1, NULL) != 0) // basicamente um wait para threads, p/ processo n acabar antes da thread
 		write(2, "Error\n", 6);
