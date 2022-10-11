184240edaddfcfeacebc9ae2dd56dd8d0e9722a6
Vitor Sergio <vitorsergio111@gmail.com>
2022-10-10 23:17:43 -0300

    init

--- philo/src/philosophers.c
+++ philo/src/philosophers.c
@@ -6,8 +6,12 @@
 /*   By: vsergio <vsergio@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 14:17:36 by vsergio           #+#    #+#             */
-/*   Updated: 2022/10/10 14:17:38 by vsergio          ###   ########.fr       */
+/*   Updated: 2022/10/10 23:08:22 by Vitor            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
+#include "../include/philosophers.h"
+int main(void)
+{
+	
+}
