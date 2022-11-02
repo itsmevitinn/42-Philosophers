d2f2401e477ab2718def7c69f6d003ffdfeabceb
Vitor Sergio <vitorsergio111@gmail.com>
2022-11-02 17:32:15 -0300

    changed mutex position at lifetime

--- philo/src/lifetime.c
+++ philo/src/lifetime.c
@@ -6,7 +6,7 @@
 /*   By: Vitor <vsergio@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 19:49:55 by Vitor             #+#    #+#             */
-/*   Updated: 2022/11/01 21:57:44 by Vitor            ###   ########.fr       */
+/*   Updated: 2022/11/02 16:50:37 by Vitor            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../include/philosophers.h"
@@ -30,17 +30,17 @@ int	monitor(t_data *data, int i)
 {
 	while (++i < data->guests)
 	{
-		pthread_mutex_lock(&data->meal_access[i]);
 		if (data->times_must_eat)
 		{
+			pthread_mutex_lock(&data->meal_access[i]);
 			if (data->meals[i] >= data->times_must_eat)
 				data->all_eaten++;
 			else
 				data->all_eaten = 0;
 			if (data->all_eaten >= data->guests)
 				return (0);
+			pthread_mutex_unlock(&data->meal_access[i]);
 		}
-		pthread_mutex_unlock(&data->meal_access[i]);
 		if (death_time(data, i))
 			return (0);
 	}
